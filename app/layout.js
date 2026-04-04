22459b4e4b6eaf0f77a7e7d9b8fab09fae9610fa
Goodcitizens <nik@goodcitizens.com.au>
2026-04-04 12:37:59 +1100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,10 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600'],
+});
 
 export const metadata = {
   title: 'hello idea',
@@ -8,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
