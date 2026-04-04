fca1ad0a60ab528c7641fc6d12d3435965f4cee6
Goodcitizens <nik@goodcitizens.com.au>
2026-04-04 15:03:28 +1100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,25 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
 
 export const metadata = {
   title: 'hello idea',
   description: 'Write it messy. Get clarity, purpose and perspective.',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
